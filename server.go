abbe29995fcb499f97a2fa4497271e06f2ad2f26
pedro simoes <pedro.simoes@doutorfinancas.pt>
2023-05-16 20:38:26 +0100

    server.go

--- server.go
+++ server.go
@@ -2,38 +2,37 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/tyler-sommer/stick"
+	"io"
+	"os"
 )
 
-type Values struct {
-	AppVersion string
-	Row        string
+func login(c *gin.Context) {
+
+	fsRoot, err := os.Getwd() // Templates are loaded relative to this directory.
+	if err == nil {
+		fmt.Println("error", err)
+	}
+	env := stick.New(stick.NewFilesystemLoader(fsRoot))
+	p := map[string]stick.Value{"name": "World"}
+	env.Execute("templates/login.html.twig", c.Writer, p)
 }
 
+func index(c *gin.Context) {
+	data, _ := io.ReadAll(c.Request.Body)
+
+	fmt.Println("test")
+	fsRoot, _ := os.Getwd() // Templates are loaded relative to this directory.
+	env := stick.New(stick.NewFilesystemLoader(fsRoot))
+	env.Execute("templates/index.html.twig", c.Writer, nil)
+
+	fmt.Println(string(data))
+
+}
 func main() {
-	log.SetFlags(log.Lshortfile)
-
-	appVersion := "bad versions with bad developer loooool"
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("index.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		data := Values{
-			AppVersion: appVersion,
-			Row:        string("test 123"),
-		}
-
-		err = tmpl.Execute(w, data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
-	fmt.Println("http://127.0.0.1:3000")
-	log.Fatalln(http.ListenAndServe("127.0.0.1:3000", nil))
+	server := gin.Default()
+	server.GET("/", login)
+	server.POST("/submmited", index)
+	server.Run(":8080")
 }
